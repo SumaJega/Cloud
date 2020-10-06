396ba69dd71f92e25284b1fe3b042522be01f178
jega <jega@gmail.com>
2020-10-06 20:12:24 +0530

    Second commit

--- Hello.java
+++ Hello.java
@@ -4,8 +4,9 @@ public class Hello
     public static void main(String args[])
     {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Changes after First commit!");
         System.out.println("Enter Your Name:");
         String name =  sc.next();
-        System.out.println("Hello "+name+ "!!!");
+        System.out.println("Hi "+name+ "!!!");
     }
 }
